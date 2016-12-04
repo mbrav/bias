87840674614f575630f5bedcac55df5eb024f477
mbrav <mbrav@protonmail.com>
2016-12-04 18:09:44 -0500

    jhsfkjhs

--- servers/main-node.js
+++ servers/main-node.js
@@ -2,50 +2,69 @@
 //created by Michael Braverman on November 12, 2016
 
 var port = 3000;
-var server = require( 'http' ).createServer( ).listen( port, function () {
-    console.log('MASTER SERVER STARTED');
-} );
-var io = require( 'socket.io' ).listen( server );
+var server = require('http').createServer().listen(port, function() {
+  console.log('MASTER SERVER STARTED');
+});
+var io = require('socket.io').listen(server);
 
 var SerialPort = require('serialport'),
-//serialPort = new SerialPort('/dev/ttyUSB0', {
-serialPort = new SerialPort('/dev/ttyS0', {
-  baudrate: 19200
-}),
-Printer = require('thermalprinter');
+  //serialPort = new SerialPort('/dev/ttyUSB0', {
+  serialPort = new SerialPort('/dev/serial0', {
+    baudrate: 19200
+  }),
+  Printer = require('thermalprinter');
 
-io.sockets.on( "connection", function ( node ) {
+io.sockets.on("connection", function(node) {
   console.log("SOCKET CONNECTED");
-  node.on( "tokens1", function (tokens) {
+  node.on("tokens1", function(tokens) {});
+  node.on("tokens2", function(tokens) {});
+  node.on("txtBuffer1", function(buffer) {
+    // printerPrint("test");
   });
-  node.on( "tokens2", function (tokens) {
-  });
-  node.on( "txtBuffer1", function (buffer) {
-    printerPrint("test");
-  });
-  node.on( "txtBuffer2", function (buffer) {
-  });
+  node.on("txtBuffer2", function(buffer) {});
 });
 
 
-function printerPrint(string) {
-  console.log("PRINTER printing: ", string);
-  serialPort.on('open', function() {
-    console.log("PRINTER open");
-    var printer = new Printer(serialPort);
-    printer.on('ready', function() {
-      onsole.log("PRINTER ready");
-      console.log("PRINTING: ", string);
-      printer
-        .indent(10)
-        .horizontalLine(16)
-        .bold(true)
-        .indent(10)
-        .printLine(string) // print passed string
-        .print(function() {
-          console.log('done');
-          process.exit();
-        });
-    });
+// function printerPrint(string) {
+//   console.log("PRINTER printing: ", string);
+//   serialPort.on('open', function() {
+//     console.log("PRINTER open");
+//     var printer = new Printer(serialPort);
+//     printer.on('ready', function() {
+//       onsole.log("PRINTER ready");
+//       console.log("PRINTING: ", string);
+//       printer
+//         .indent(10)
+//         .horizontalLine(16)
+//         .bold(true)
+//         .indent(10)
+//         .printLine(string) // print passed string
+//         .print(function() {
+//           console.log('done');
+//           process.exit();
+//         });
+//     });
+//   });
+// }
+
+serialPort.on('open', function() {
+  var printer = new Printer(serialPort);
+  printer.on('ready', function() {
+    printer
+      .indent(10)
+      .horizontalLine(16)
+      .bold(true)
+      .indent(10)
+      .printLine('whatsaup!')
+      .bold(false)
+      .inverse(true)
+      .big(true)
+      .right()
+      .printLine('second line')
+      //.printImage(path)
+      .print(function() {
+        console.log('done');
+        process.exit();
+      });
   });
-}
+});
