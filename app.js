81796bd47ddfa6361232aec43f9196b4a14fb214
mbrav <mbrav@protonmail.com>
2016-12-10 00:53:34 -0500

    test2

--- app.js
+++ app.js
@@ -177,7 +177,7 @@ function socketStreamSetup() {
     lcd.on('ready', function() {
       lcd.setCursor(0, 0);
       lcd.print(topics[topicId].topic + ": " + topics[topicId].tokens[tokenId]);
-    }
+    });
     // twitter streAMS
     stream1 = T.stream('statuses/filter', {
       track: topics[topicId].tokens[tokenId]
