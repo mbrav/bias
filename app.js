a2c4c7c2f0be74b995da3943e0b88a812a7a9255
mbrav <mbrav@protonmail.com>
2016-12-11 17:22:54 -0500

    ops4

--- app.js
+++ app.js
@@ -212,6 +212,18 @@ function socketStreamSetup() {
   			});
   		});
     });
+    lcd.clear(function () {
+      lcd.setCursor(0, 0);
+      lcd.print(topics[topicId].topic + ":");
+      lcd.once('printed', function() {
+        lcd.setCursor(0, 1);
+        lcd.print(topics[topicId].tokens[tokenId]);
+        lcd.once('printed', function() {
+          // lcd.clear();
+          // lcd.close();
+        });
+      });
+    });
     // twitter streAMS
     stream1 = T.stream('statuses/filter', {
       track: topics[topicId].tokens[tokenId]
