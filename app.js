a90aec53fc82e3a31b69e72d8417aefb14870016
mbrav <mbrav@protonmail.com>
2016-12-11 13:39:26 -0500

    back to google

--- app.js
+++ app.js
@@ -351,8 +351,8 @@ function speak(text) {
   // talk only when not currently talking, avoid overlaping
   if (!talking) {
     talking = true;
-    var cmdText = 'espeak -s50 -p160 -g8 "' + text + '"';
-    // var cmdText = './googleTTS.sh ' + text;
+    // var cmdText = 'espeak -s50 -p160 -g8 "' + text + '"';
+    var cmdText = './googleTTS.sh ' + text;
     // var cmdText = 'say -v Samantha -r 2000 "' + text + '"'; // for macOS only
     cmd = childProcess.exec(cmdText, function(error, stdout, stderr) {
       console.log("voice ON");
